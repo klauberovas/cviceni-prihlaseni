c70a14b4c8ef8a7606af960d82a98008cf446042
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-02 15:26:48 +0200

    oprava

--- script.js
+++ script.js
@@ -5,34 +5,43 @@ const user = {
 };
 const formElm = document.querySelector('form');
 const buttonElm = document.querySelector('button');
-let nameElm = document.getElementById('name');
-let passwordElm = document.getElementById('password');
-let emailElm = document.getElementById('email');
+let nameInputElm = document.getElementById('name');
+let passwordInputElm = document.getElementById('password');
+let emailInputElm = document.getElementById('email');
 const messageElm = document.querySelector('.message');
 
-const checkInput = (event) => {
-  if (nameElm.value === '' || !emailElm.value.includes('@')) {
-    return event.target.classList.add('input--red');
+const checkInput = (event, condition) => {
+  if (condition) {
+    event.target.classList.add('input--red');
+  } else {
+    event.target.classList.remove('input--red');
   }
-  return event.target.classList.remove('input--red');
 };
 
-nameElm.addEventListener('input', checkInput);
-emailElm.addEventListener('input', checkInput);
+nameInputElm.addEventListener('input', (event) => {
+  checkInput(event, nameInputElm.value === '');
+});
+
+emailInputElm.addEventListener('input', (event) => {
+  checkInput(event, !emailInputElm.value.includes('@'));
+});
 
 buttonElm.addEventListener('click', (e) => {
   e.preventDefault();
 
-  if (user.password === passwordElm.value && user.name === nameElm.value) {
-    return (formElm.innerHTML = `<h2>Přihlášený uživatel ${nameElm.value}</h2>`);
+  if (
+    user.password === passwordInputElm.value &&
+    user.name === nameInputElm.value
+  ) {
+    return (formElm.innerHTML = `<h2>Přihlášený uživatel ${nameInputElm.value}</h2>`);
   } else if (
-    user.email !== emailElm.value &&
-    user.password !== passwordElm.value
+    user.email !== emailInputElm.value &&
+    user.password !== passwordInputElm.value
   ) {
     return (
       (messageElm.innerHTML = '<h2>Neplatné přihlašovací údaje</h>'),
-      (nameElm.value = ''),
-      (passwordElm.value = '')
+      (nameInputElm.value = ''),
+      (passwordInputElm.value = '')
     );
   }
 });
